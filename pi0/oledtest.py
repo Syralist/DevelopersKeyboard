cd64fba99b5947895d9feec6cbe66ba93c2f2b78
Thomas Helmke <thomas.helmke@gmx.de>
2018-03-29 22:10:15 +0200

    erster test mit oled display

---
+++ pi0/oledtest.py
@@ -0,0 +1,39 @@
+from Adafruit_SSD1306 import SSD1306_128_64 as OLED
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+disp = OLED(rst=24, i2c_address=0x3C)
+
+# Initialize library.
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+# Load default font.
+font = ImageFont.load_default()
+
+# Write two lines of text.
+x = 2
+top = 2
+draw.text((x, top),    'Hello',  font=font, fill=255)
+draw.text((x, top+20), 'World!', font=font, fill=255)
+
+# Display image.
+disp.image(image)
+disp.display()
